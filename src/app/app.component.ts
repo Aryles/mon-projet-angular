7086e046e27f8537a1db8ccdae954ac294320e23
Aryles <talbinearyles@gmail.com>
2019-06-27 17:51:39 +0200

    Commit1
    Rajout des fonctionalité de base Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+isAuth =false;
+App1 ="Machine a laver";
+App2 ="Televison";
+App3 ="Telephone";
+constructor() {
+  setTimeout(
+() => {
+this.isAuth= true;
+}, 4000
+  );
+}
+onAllumer(){
+  console.log('On allume tout !');
+}
+
+
 }
